a14860eb12a69cbfc8f9b7fde7632b95ba42a6bb
Alevtina71 <a.a.reppa@gmail.com>
2022-05-19 21:32:24 +0300

    Update to 6 lesson

--- src/test/java/lesson6/MainTest.java
+++ src/test/java/lesson6/MainTest.java
@@ -10,8 +10,7 @@ public class MainTest extends AbstractTest{
     void ReviewBin(){
         MainMenu mainMenu = new MainMenu(getWebDriver());
         mainMenu.ReviewBin();
-
-        Assertions.assertTrue(getWebDriver().findElement(By.xpath("//p[@id='cart-buttons']/a")).getText().equals("Ваша корзина пуста"), "Успешно");
+        Assertions.assertEquals("Ваша корзина пуста", getWebDriver().findElement(By.xpath("//p[@id='cart-buttons']/a")).getText());
     }
 
     @Test
@@ -19,9 +18,9 @@ public class MainTest extends AbstractTest{
         MainMenu mainMenu = new MainMenu(getWebDriver());
         mainMenu.AddSomeItems();
         Assertions.assertTrue(getWebDriver().getCurrentUrl().contains("https://www.grekoliva.ru/"));
-        Assertions.assertTrue(getWebDriver().findElement(By.xpath("//*[@id=\"center_column\"]/div[2]/div[1]/h5/a")).getText().equals("Оливковое масло"));
-        Assertions.assertTrue(getWebDriver().findElement(By.xpath("//*[@id=\"subcategories\"]/ul/li[1]/a[2]")).getText().equals("Оливковое масло Olivi Platinum нефильтрованное, Греция, ст.бут., 500мл"));
-        Assertions.assertTrue(getWebDriver().findElement(By.xpath("//*[@id=\"cart_block_total\"]")).getText().equals("5 400 руб."), "Масло успешно добавлено в корзину");
+        Assertions.assertEquals("Оливковое масло", getWebDriver().findElement(By.xpath("//*[@id=\"center_column\"]/div[2]/div[1]/h5/a")).getText());
+        Assertions.assertEquals("Оливковое масло Olivi Platinum нефильтрованное, Греция, ст.бут., 500мл", getWebDriver().findElement(By.xpath("//*[@id=\"subcategories\"]/ul/li[1]/a[2]")).getText());
+        Assertions.assertEquals("5 400 руб.", getWebDriver().findElement(By.xpath("//*[@id=\"cart_block_total\"]")).getText(), "Масло успешно добавлено в корзину");
     }
 
     @Test
@@ -29,7 +28,7 @@ public class MainTest extends AbstractTest{
         MainMenu mainMenu = new MainMenu(getWebDriver());
         mainMenu.AddPremiumOil();
         Assertions.assertTrue(getWebDriver().getCurrentUrl().contains("https://www.grekoliva.ru/"));
-        Assertions.assertTrue(getWebDriver().findElement(By.xpath("//*[@id=\"center_column\"]/div[2]/div[1]/h5/a")).getText().equals("Оливковое масло"));
-        Assertions.assertTrue(getWebDriver().findElement(By.xpath("//div[@id='subcategories']/ul/li/a[2]")).getText().equals("Ваша корзина пуста"), "PREMIUM ORGANIC");
+        Assertions.assertEquals("Оливковое масло", getWebDriver().findElement(By.xpath("//*[@id=\"center_column\"]/div[2]/div[1]/h5/a")).getText());
+        Assertions.assertEquals("Ваша корзина пуста", getWebDriver().findElement(By.xpath("//div[@id='subcategories']/ul/li/a[2]")).getText(), "PREMIUM ORGANIC");
     }
 }
